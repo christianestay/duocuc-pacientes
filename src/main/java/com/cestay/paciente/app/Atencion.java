3b51cffc460d00f3dc336baa7f697ddfd8082342
Christian Estay <cestay@atomix.cl>
2024-03-25 14:31:59 -0300

    v1

--- src/main/java/com/cestay/paciente/app/Atencion.java
+++ src/main/java/com/cestay/paciente/app/Atencion.java
@@ -1,5 +1,47 @@
-package com;
+package com.cestay.paciente.app;
+
+import java.util.Date;
 
 public class Atencion {
+
+    private Date fecha;
+    private String medico;
+    private String diagnostico;
+
+    public Atencion() {
+    
+    }
+
+    public Atencion(Date fecha, String medico, String diagnostico) {
+        this.fecha = fecha;
+        this.medico = medico;
+        this.diagnostico = diagnostico;
+    }
+
+    public Date getFecha() {
+        return this.fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMedico() {
+        return this.medico;
+    }
+
+    public void setMedico(String medico) {
+        this.medico = medico;
+    }
+
+    public String getDiagnostico() {
+        return this.diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+
 
 }
